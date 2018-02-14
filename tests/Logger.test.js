da1cdf17de3fc4d69eeca78dec60d9843c198ec9
SergejsK <sergej@colourbox.com>
2018-02-14 21:55:16 +0100

    Update Logger.test.js

--- tests/Logger.test.js
+++ tests/Logger.test.js
@@ -7,6 +7,5 @@ test('Test logger creates a file and adds a string', () => {
     const string = 'some string';
 
     const fileReader = new FileReader('logs.txt');
-
     expect(fileReader.read()).toEqual([string]);
 });
